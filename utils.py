efc107db25b39829dba7191ce646b772f2eb9fd8
Palandr1234 <palandr545@gmail.com>
2022-05-24 10:51:24 +0900

    remove normalize

--- utils.py
+++ utils.py
@@ -2,12 +2,6 @@ import numpy as np
 from PIL import Image
 
 
-def normalize(image, mean, std):
-    image = image.transpose(2, 0, 1)
-    image = (image - mean[:, None, None]) / std[:, None, None]
-    return image.transpose(1, 2, 0)
-
-
 def apply_operation(image, operation):
     # rescale the image to the range [0, 255]
     image = np.clip(image * 255., 0, 255).astype(np.uint8)
