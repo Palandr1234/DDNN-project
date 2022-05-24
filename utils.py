591812792446e6f34b3bc7bb93e868f1fecd35af
Palandr1234 <palandr545@gmail.com>
2022-05-24 11:59:10 +0900

    add normalize

--- utils.py
+++ utils.py
@@ -2,6 +2,18 @@ import numpy as np
 from PIL import Image
 
 
+# CIFAR-10 constants
+MEAN = [0.4914, 0.4822, 0.4465]
+STD = [0.2023, 0.1994, 0.2010]
+
+
+def normalize(image, mean=MEAN, std=STD):
+    image = image.transpose(2, 0, 1)
+    mean, std = np.array(mean), np.array(std)
+    image = (image - mean[:, None, None]) / std[:, None, None]
+    return image.transpose(1, 2, 0)
+
+
 def apply_operation(image, operation):
     # rescale the image to the range [0, 255]
     image = np.clip(image * 255., 0, 255).astype(np.uint8)
